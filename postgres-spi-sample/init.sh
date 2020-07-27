175bd110185c14f3aa16c42afc4fc461ba105f65
axt <axt_star@hotmail.com>
2020-07-28 00:12:35 +0900

    fix

--- postgres-spi-sample/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo '************************init start***************************'
-echo $dynamic_library_path 
-# http://pgsql-jp.github.io/current/html/runtime-config-client.html#GUC-DYNAMIC-LIBRARY-PATH
-export dynamic_library_path=/usr/local/lib/postgresql:/usr/local/lib/
-echo $dynamic_library_path
-echo '************************init   end***************************'
